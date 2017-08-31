52e8f14bb8278d216c512f186356f48929e8c218
silviocedrim <engenheirocedrim@gmail.com>
2017-08-31 11:01:42 -0300

    Add projeto WEB

---
+++ project_drinks/project_drinks_web/src/dados/DaoGenerico.java
@@ -0,0 +1,140 @@
+package dados;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+public abstract class DaoGenerico<Entidade> implements IDaoGenerico<Entidade>{
+
+	protected EntityManager entityManager;
+	protected Class<Entidade> classePersistente;
+
+	@SuppressWarnings("unchecked")
+	public DaoGenerico(EntityManager em){
+		this.entityManager = em;
+		ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();  
+	    classePersistente = (Class<Entidade>) parameterizedType.getActualTypeArguments()[0];  
+	}
+	
+	public final void alterar(Entidade objeto) {
+		EntityTransaction tx = getEntityManager().getTransaction();
+		try {
+			tx.begin();
+			 
+			objeto = getEntityManager().merge(objeto);
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()){
+				tx.rollback();
+			}
+		}
+	}
+
+	public final void inserir(Entidade objeto) {
+		EntityTransaction tx = getEntityManager().getTransaction();		
+		try {
+			tx.begin();
+			getEntityManager().persist(objeto);
+			tx.commit();
+			System.out.println(classePersistente.getSimpleName() + " salvo com sucesso");
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()){
+				tx.rollback();
+			}
+		}
+	}
+
+
+	public final void inserirColecao(Collection<Entidade> colecao) {
+		EntityTransaction tx = getEntityManager().getTransaction();
+		try {
+			tx.begin();
+
+			for (Entidade entidade : colecao) {
+				getEntityManager().persist(entidade);	
+			}
+			
+			tx.commit();
+			
+			System.out.println(classePersistente.getSimpleName() + " salvos com sucesso: " + colecao.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()){
+				tx.rollback();
+			}
+		}
+	}
+
+	public final void remover(Entidade objeto) {
+		EntityTransaction tx = getEntityManager().getTransaction();
+		try {
+			tx.begin();
+	
+			objeto = getEntityManager().merge(objeto);
+			getEntityManager().remove(objeto);
+			
+			tx.commit();
+			
+			System.out.println(classePersistente.getSimpleName() + " removido com sucesso");
+		} catch (Exception e){
+			e.printStackTrace();
+			if (tx != null && tx.isActive()){
+				tx.rollback();
+			}
+		}
+	}
+
+	
+	public final Entidade consultarPorId(Integer chave) {
+		Entidade instance = null;
+		try {
+			instance = (Entidade) getEntityManager().find(classePersistente, chave);
+		} catch (RuntimeException re) {
+			re.printStackTrace();
+		}
+		return instance;
+	}
+
+	public List<Entidade> consultarTodos() {
+		try {
+			String sql = "from " + classePersistente.getSimpleName();
+			TypedQuery<Entidade> query = entityManager.createQuery(sql, classePersistente);
+			return query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public List<Entidade> consultarTodos(Integer indiceInicial,
+			Integer quantidade) {
+		try {
+			String sql = "from " + classePersistente.getSimpleName();
+			TypedQuery<Entidade> query = entityManager.createQuery(sql, classePersistente);
+			query = query.setFirstResult(indiceInicial).setMaxResults(quantidade);
+			return query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+		
+}
