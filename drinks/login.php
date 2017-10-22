2059f5a6f73c6aecaa02865b5658b2b6f4e055e9
Silvio Cedrim <engenheirocedrim@gmail.com>
2017-10-22 12:17:19 -0300

    Layout.

--- drinks/login.php
+++ drinks/login.php
@@ -23,8 +23,9 @@ if (isset($_POST['email']) && empty($_POST['email']) == false) {
         
         header("Location: administrador/estabelecimento/cadastro-estabelecimentos.php");
     }else{
-        echo '<div class="alert alert-danger" role="alert">';
-        echo '<strong>Aviso!</strong> Verifique se digitou o login corretamente.';
+        echo '<div class="alert alert-danger alert-dismissible" role="alert">';
+        echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+        echo '<strong>Aviso!</strong> Login ou Senha inv&aacute;lidos.';
         echo '</div>';
     }
     
@@ -32,69 +33,72 @@ if (isset($_POST['email']) && empty($_POST['email']) == false) {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=euc-jp">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-	rel="stylesheet">
-<link rel="stylesheet" type="text/css"
-	href="biblioteca/materialize/css/materialize.min.css">
-<link href="css/login.css" rel="stylesheet">
-</head>
+    <head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=euc-jp">
+    	<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        
+        <!-- CSS -->
+        <link href="theme.css" rel="stylesheet">
+    	<link href="jumbotron.css" rel="stylesheet">
+        <link href="biblioteca/bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="biblioteca/bootstrap-3.3.7/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="biblioteca/css/estilo.css" rel="stylesheet" />        
+        <link href="biblioteca/materialize/css/materialize.min.css" rel="stylesheet" type="text/css" >
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        
+        <script src="biblioteca/jquery/jquery-1.5.2.min.js"></script>
+    	<script src="biblioteca/jquery/jquery.maskedinput-1.3.min.js"></script>
+        <script src="biblioteca/bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
+       
+        <link type="text/css" rel="stylesheet" href="biblioteca/css/ui.css?ln=css" />
+        <link type="text/css" rel="stylesheet" href="biblioteca/css/messages.css?ln=css" />
+        <link type="text/css" rel="stylesheet" href="biblioteca/css/bootstrap.css?ln=css" />
+        
+    </head>
 
 <body>
-	<div class="section"></div>
-	<main>
-	<center>
-		<div class="section"></div>
 
-		<h5 class="indigo-text">Por favor, entre na sua conta</h5>
-		<div class="section"></div>
-
-		<div class="container">
-			<div class="z-depth-1 grey lighten-5 row"
-				style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
-
-				<form class="col s12" method="POST">
-					<div class='row'>
-						<div class='col s12'></div>
-					</div>
-
-					<div class='row'>
-						<div class='input-field col s12'>
-							<input class='validate' type='email' name='email' id='email' required/> <label
-								for='email' >Coloque seu email</label>
-						</div>
-					</div>
-
-					<div class='row'>
-						<div class='input-field col s12'>
-							<input class='validate' type='password' name='senha'
-								id='password' required/> <label for='password'>Coloque sua senha</label>
-						</div>
-						<!-- <label style='float: right;'> <a class='pink-text' href='#!'><b>Esqueceu a senha?</b></a></label> -->
-
-						
-					</div>
-
-					<br />
-					<div class='row'>
-						<button type='submit' name='btn_login'
-							class='col s12 btn btn-large waves-effect indigo'>Entrar</button>
-					</div>
-				</form>
-			</div>
-		</div>
-		<!-- <a href="#!">Create account</a> -->
-	</center>
-
-	<div class="section"></div>
-	<div class="section"></div>
-	</main>
-
-	<script type="text/javascript"
-		src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-	<script type="text/javascript"
-		src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+		<div id="wrapper">
+    		<div class="panel-body"></div>
+    		
+        	<div class="container">
+    			<div class="row">
+				<div class="col-md-3 col-md-offset-2 text-center">
+					<img src="biblioteca/img/logo_marca_drinks.png?ln=images" width="170" style="margin-top: 20%;" />
+				</div>    			
+    			
+                	<div class="col-md-4" style="margin-top: 6%;">
+                    	<div class="login-panel panel panel-default">
+                    		<div class="panel-heading">
+                    			<h3 class="panel-title">Login</h3>
+                    		</div>
+                    		
+                    		<form class="col s12" method="POST" class="login-form">
+                        		<div class="panel-body">
+                    				<div class="form-group input-group">
+                    					<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    					 <input id="email" name="email" type="email" placeholder="Informe o nome de usu&aacute;rio" class="form-control" required/>
+                    				</div>
+                    
+                    				<div class="form-group input-group">
+                    					<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span> 
+                    					<input id="senha" name="senha" type="password" placeholder="Informe a senha" class="form-control" />
+                    				</div>
+                    				
+                    				<input tabindex="2" type="submit" value="Entrar" class="btn btn-lg btn-primary btn-block" style="text-transform: uppercase;" required/>
+                        		</div>
+                    		</form>
+                    		
+                    	</div>
+                	</div>
+        		</div>
+        	</div>
+        </div>
 </body>
 
 </html>
