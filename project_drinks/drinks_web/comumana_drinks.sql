6cfe406ee34d8950ee2f467cc1d0b7408bcc9be5
TercioLima <terciosky@gmail.com>
2017-11-05 19:08:02 -0300

    Correção de Bug ao efetuar Alter Table para tabelas `pedido` e `produto`;

--- project_drinks/drinks_web/comumana_drinks.sql
+++ project_drinks/drinks_web/comumana_drinks.sql
@@ -23,6 +23,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `codUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `encrypted_password` varchar(80) NOT NULL,
+  `telefone` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`codUsuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Fazendo dump de dados para tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`codUsuario`, `nome`, `email`, `encrypted_password`, `telefone`) VALUES
+(1, 'Emerson Francisco', 'ems@gmail.com', 'jLIjfQZ5yojbZGTqxg2pY0VROWQ=', '81986854814'),
+(2, 'Tercio Lima', 'terciosky@gmail.com', 'QL0AFWMIX8NRZTKeof9cXsvbvu8=', '81996644179'),
+(3, 'Juliana', 'souza3408@gmail.com', 'a15q4UVZBErnR7WyvhHqQ2Sq/p0=', '81986226795'),
+(4, 'Silvio', 'engeheirocedrim@gmail.com', 'fEqNCco3Yq9h5ZUglD3CZJT4lBs=', '81998237293'),
+(5, 'italo', 'italo@italo.com', 'fEqNCco3Yq9h5ZUglD3CZJT4lBs=', '81995974462');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `estabelecimento`
 --
 
@@ -59,7 +85,51 @@ INSERT INTO `estabelecimento` (`codEstabelecimento`, `cnpj`, `eMail`, `login`, `
 (4, '10636838000120', 'grutabar.camaragibe@gmail.com', 'grutabar', 'Vila da Fábrica', '54759135', 'Camaragibe', '-8.0139369', '-34.9788453', 387, 'Rua Carlos Alberto de Menezes', 'PE', 'GRUTA BAR', 'M J DE SOUZA BAR - ME', '123456', '81999998146', 0),
 (5, '12972637000129', 'betosbaroficial@gmail.com', 'betosbar', 'Candeias', '54440620', 'Jaboatao Dos Guararapes', '-8.0139369', '-34.9788453', 4738, 'Av. Bernardo Vieira De Melo', 'PE', 'BetoS Bar', 'BetoS Restaurante e Pizzaria Ltda - Epp', '123456', '8134692995', 0),
 (6, '09202813000139', 'bardobarriga@gmail.com', 'bardobarriga', 'Pilar', '53900000', 'Ilha de Itamaracá', '-8.0139369', '-34.9788453', 30, 'Rua Assunção', 'PE', 'Bar Do Barriga O Rei Da Tainha', 'Bar Do Barriga O Rei Da Tainha', 'c20ad4d76fe97759', '81991915773', 0);
---Bar Do Barriga O Rei Da Tainha terá dados fictícios, pois não foram encontrados na rede;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE IF NOT EXISTS `produto` (
+  `codProduto` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(400) NOT NULL,
+  `gelada` tinyint(1) DEFAULT NULL,
+  `nome` varchar(150) NOT NULL,
+  `preco` double NOT NULL,
+  `codEstabelecimento` int(11) NOT NULL,
+  PRIMARY KEY (`codProduto`),
+  KEY `FK_Produto_Estabelecimento` (`codEstabelecimento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Fazendo dump de dados para tabela `produto`
+--
+
+INSERT INTO `produto` (`codProduto`, `descricao`, `gelada`, `nome`, `preco`, `codEstabelecimento`) VALUES
+(1, 'Gelo', 0, 'Gelo em Cubos', 3.99, 2),
+(2, 'Cerveja ANTARCTICA Pilsen Lata 350ml', 0, 'Cerveja ANTARCTICA 350mL', 2.79, 2),
+(3, 'Refrigerante Fanta Lata gelada ', 0, 'Fanta - Lata', 3.59, 2),
+(4, 'Refrigerante Coca-cola Lata gelada ', 0, 'Coca-Cola Lata 350mL', 3.59, 2),
+(5, 'Cerveja KAISER Lager Pilsen Lata 350ml', 0, 'Cerveja KAISER 350mL', 1.79, 2),
+(6, 'Cerveja HEINEKEN Lager Beer Premium Lata 350ml', 0, 'Cerveja HEINEKEN 350mL', 3.59, 2),
+(7, 'Cerveja DEVASSA Tropical Lager Long Neck 355mla', 0, 'Cerveja DEVASSA 355mL', 3.20, 2),
+(8, 'Gelo', 0, 'Gelo em Escamas', 2.99, 3),
+(9, 'Cerveja ANTARCTICA Pilsen Lata 350ml', 0, 'Cerveja ANTARCTICA 350mL', 2.69, 3),
+(10, 'Vinho Tinto Seco 750 mL Salton Classic Cabernet Sauvignon 2016', 1, 'Vinho Tinto Seco 750 mL', 25.90, 3),
+(11, 'Vinho Branco Seco 750mL Miolo Seleção Chardonnay Viognier 2016', 1, 'Vinho Branco Seco 750mL', 35.59, 3),
+(12, 'Refrigerante Coca-cola 2L gelada', 0, 'Coca-Cola - 2L', 5.19, 3),
+(13, 'Cerveja DEVASSA 350mL', 0, 'Cerveja DEVASSA Tropical Lager Lata 350ml', 1.99, 3),
+(14, 'Refrigerante Sprite 2L gelada', 0, 'Sprite - 2L', 3.45, 3),
+(15, 'Gelo', 0, 'Gelo em Cubos', 3.50, 5),
+(16, 'Gelo', 0, 'Gelo em Escamas', 3.99, 5),
+(17, 'Pitu', 1, 'Cachaça Pitu', 10.59, 5),
+(18, 'Cerveja BOHEMIA Pilsen Long Neck 355ml', 0, 'Cerveja BOHEMIA 350mL', 3.49, 5),
+(19, 'Refrigerante Coca-cola 2L gelada', 0, 'Coca-Cola - 2L', 6.49, 3),
+(20, 'Vinho Bodega Vieja Tinto Suave 750mL', 1, 'Vinho Tinto Suave 750mL', 35.50, 5),
+(22, 'Refrigerante Sprite 2L gelada', 0, 'Sprite - 2L', 4.00, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -92,8 +162,8 @@ CREATE TABLE IF NOT EXISTS `pedido` (
 --
 
 INSERT INTO `pedido` (`codPedido`, `dataPedido`, `bairro`, `cep`, `cidade`, `latitude`, `longitude`, `numero`, `rua`, `uf`, `pagamento`, `status`, `valorTotal`, `codUsuario`, `codEstabelecimento`) VALUES
-(1, '31/10/2017 19:16:35', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ESPECIE', 'AGUARDANDO', 25.57, 16, 5),
-(2, '31/10/2017 19:28:53', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ESPECIE', 'AGUARDANDO', 40.74, 16, 2),
+(1, '31/10/2017 19:16:35', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ESPECIE', 'AGUARDANDO', 25.57, 5, 5),
+(2, '31/10/2017 19:28:53', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ESPECIE', 'AGUARDANDO', 40.74, 4, 2),
 (3, '01/11/2017 20:18:12', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'CARTAO', 'AGUARDANDO', 81.15, 2, 3),
 (4, '05/11/2017 19:05:25', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'CARTAO', 'AGUARDANDO', 66.93, 1, 5);
 
@@ -118,98 +188,18 @@ CREATE TABLE IF NOT EXISTS `pedido_produto` (
 --
 
 INSERT INTO `pedido_produto` (`codPedido`, `codProduto`, `preco`, `quantidade`, `precoTotal`) VALUES
-(1, 1, 3.99, 1, 3.99),
+(1, 16, 3.99, 1, 3.99),
 (1, 7, 3.20, 5, 16.00),
 (1, 2, 2.79, 2, 5.58),
 (2, 7, 3.20, 6, 19.20),
-(2, 6, 3.59, 6, 21.54),
-(3, 3, 25.90, 3, 77.70),
-(3, 7, 3.45, 1, 3.45),
-(4, 2, 3.99, 1, 3.99),
-(4, 7, 6.49, 6, 38.94),
-(4, 5, 4.00, 6, 24.00);
+(2, 4, 3.59, 6, 21.54),
+(3, 10, 25.90, 3, 77.70),
+(3, 14, 3.45, 1, 3.45),
+(4, 16, 3.99, 1, 3.99),
+(4, 19, 6.49, 6, 38.94),
+(4, 22, 4.00, 6, 24.00);
 
 -- --------------------------------------------------------
-
---
--- Estrutura para tabela `produto`
---
-
-CREATE TABLE IF NOT EXISTS `produto` (
-  `codProduto` int(11) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(400) NOT NULL,
-  `gelada` tinyint(1) DEFAULT NULL,
-  `nome` varchar(150) NOT NULL,
-  `preco` double NOT NULL,
-  `codEstabelecimento` int(11) NOT NULL,
-  PRIMARY KEY (`codProduto`),
-  KEY `FK_Produto_Estabelecimento` (`codEstabelecimento`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
-
---
--- Fazendo dump de dados para tabela `produto`
---
-
-INSERT INTO `produto` (`codProduto`, `descricao`, `gelada`, `nome`, `preco`, `codEstabelecimento`) VALUES
-(1, 'Gelo', 0, 'Gelo em Cubos', 3.99, 2),
-(2, 'Cerveja ANTARCTICA Pilsen Lata 350ml', 0, 'Cerveja ANTARCTICA 350mL', 2.79, 2),
-(3, 'Refrigerante Fanta Lata gelada ', 0, 'Fanta - Lata', 3.59, 2),
-(4, 'Refrigerante Coca-cola Lata gelada ', 0, 'Coca-Cola Lata 350mL', 3.59, 2),
-(5, 'Cerveja KAISER Lager Pilsen Lata 350ml', 0, 'Cerveja KAISER 350mL', 1.79, 2),
-(6, 'Cerveja HEINEKEN Lager Beer Premium Lata 350ml', 0, 'Cerveja HEINEKEN 350mL', 3.59, 2),
-(7, 'Cerveja DEVASSA Tropical Lager Long Neck 355mla', 0, 'Cerveja DEVASSA 355mL', 3.20, 2),
-
-(1, 'Gelo', 0, 'Gelo em Escamas', 2.99, 3),
-(2, 'Cerveja ANTARCTICA Pilsen Lata 350ml', 0, 'Cerveja ANTARCTICA 350mL', 2.69, 3),
-(3, 'Vinho Tinto Seco 750 mL Salton Classic Cabernet Sauvignon 2016', 1, 'Vinho Tinto Seco 750 mL', 25.90, 3),
-(4, 'Vinho Branco Seco 750mL Miolo Seleção Chardonnay Viognier 2016', 1, 'Vinho Branco Seco 750mL', 35.59, 3),
-(5, 'Refrigerante Coca-cola 2L gelada', 0, 'Coca-Cola - 2L', 5.19, 3),
-(6, 'Cerveja DEVASSA 350mL', 0, 'Cerveja DEVASSA Tropical Lager Lata 350ml', 1.99, 3),
-(7, 'Refrigerante Sprite 2L gelada', 0, 'Sprite - 2L', 3.45, 3);
-
-(1, 'Gelo', 0, 'Gelo em Cubos', 3.50, 5),
-(2, 'Gelo', 0, 'Gelo em Escamas', 3.99, 5),
-(3, 'Pitu ', 1, 'Cachaça Pitu', 10.59, 5),
-(4, 'Cerveja BOHEMIA Pilsen Long Neck 355ml', 0, 'Cerveja BOHEMIA 350mL', 3.49, 5),
-(5, 'Refrigerante Coca-cola 2L gelada', 0, 'Coca-Cola - 2L', 6.49, 3),
-(6, 'Vinho Bodega Vieja Tinto Suave 750mL', 1, 'Vinho Tinto Suave 750mL', 35.50, 5),
-(7, 'Refrigerante Sprite 2L gelada', 0, 'Sprite - 2L', 4.00, 5);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `codUsuario` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `encrypted_password` varchar(80) NOT NULL,
-  `telefone` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`codUsuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
-
---
--- Fazendo dump de dados para tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`codUsuario`, `nome`, `email`, `encrypted_password`, `telefone`) VALUES
-(1, 'Emerson Francisco', 'ems@gmail.com', 'jLIjfQZ5yojbZGTqxg2pY0VROWQ=', '81986854814'),
-(2, 'Tercio Lima', 'terciosky@gmail.com', 'QL0AFWMIX8NRZTKeof9cXsvbvu8=', '81996644179'),
-(3, 'Juliana', 'souza3408@gmail.com', 'a15q4UVZBErnR7WyvhHqQ2Sq/p0=', '81986226795'),
-(4, 'Silvio', 'engeheirocedrim@gmail.com', 'fEqNCco3Yq9h5ZUglD3CZJT4lBs=', '81998237293'),
-(5, 'italo', 'italo@italo.com', 'fEqNCco3Yq9h5ZUglD3CZJT4lBs=', '81995974462');
---
--- Restrições para dumps de tabelas
---
-
---
--- Restrições para tabelas `pedido`
---
-ALTER TABLE `pedido`
-  ADD CONSTRAINT `FK_codEstabelecimento` FOREIGN KEY (`codEstabelecimento`) REFERENCES `estabelecimento` (`codEstabelecimento`),
-  ADD CONSTRAINT `FK_codUsuario` FOREIGN KEY (`codUsuario`) REFERENCES `usuarios` (`codUsuario`);
 
 --
 -- Restrições para tabelas `pedido_produto`
@@ -217,12 +207,6 @@ ALTER TABLE `pedido`
 ALTER TABLE `pedido_produto`
   ADD CONSTRAINT `FK_Pedido_produto_codPedido` FOREIGN KEY (`codPedido`) REFERENCES `pedido` (`codPedido`),
   ADD CONSTRAINT `FK_Pedido_produto_codProduto` FOREIGN KEY (`codProduto`) REFERENCES `produto` (`codProduto`);
-
---
--- Restrições para tabelas `produto`
---
-ALTER TABLE `produto`
-  ADD CONSTRAINT `FK_Produto_Estabelecimento` FOREIGN KEY (`codEstabelecimento`) REFERENCES `estabelecimento` (`codEstabelecimento`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
